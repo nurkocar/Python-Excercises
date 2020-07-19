2fc4dad09cb7256eeac976219c0aaf5b3f590f76
Nur <fatmanurkocar61@gmail.com>
2020-07-19 10:38:12 +0000

    Prime numbers odevi nested loop ile yapildi

---
+++ Assignment - 6 (Prime Numbers).py
@@ -0,0 +1,13 @@
+num = int(input('Enter a limit to see the prime numbers :'))
+
+last_list = []
+for i in range(2, num + 1):
+    divisors = [1]
+    for j in range(2,i+1):
+        if i % j == 0:divisors.append(j)
+        else:continue
+    if divisors == [1,j]:last_list.append(i)
+    else:continue
+        
+        
+print(last_list)
