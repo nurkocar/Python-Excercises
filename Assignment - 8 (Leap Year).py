c986a28e5bc9f7abf85597057e648b26073a21c6
Nur <fatmanurkocar61@gmail.com>
2020-07-23 08:40:45 +0000

    Leap Year yapildi

---
+++ Assignment - 8 (Leap Year).py
@@ -0,0 +1,8 @@
+year = int(input('Enter a year to check if it is a leap year or not :'))
+if year % 4 == 0:
+    if year % 100 == 0 and year % 400 != 0:
+        print(f'{year} is not a leap year.')
+    else:
+        print(f'{year} is a leap year.')
+else:
+    print(f'{year} is not a leap year.')
