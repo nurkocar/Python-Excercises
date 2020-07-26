f2cf743b9505c31527eb0b85b46645a7c191aec5
Nur <fatmanurkocar61@gmail.com>
2020-07-26 08:27:48 +0000

    Fizz Buzz

--- Assignment_7.py
+++ Assignment_7.py
@@ -1,4 +1,4 @@
-# 1. yol
+# 1. yol:
 for i in range(1,101):
     if (i % 15 == 0):print("FizzBuzz")
     elif (i % 3 == 0):print("Fizz")
@@ -7,10 +7,10 @@ for i in range(1,101):
 
 # Yorumlar duzeltildi
 
-# 2. yol
+# 2. yol:
 # for i in range(1,101):print("Fizz" * (i % 3 == 0) + "Buzz" * (i % 5 == 0) or i)
 
-# 3. yol
+# 3. yol:
 # def fizzBuzz(last):
 #    for i in range(1,last + 1):print("Fizz" * (i % 3 == 0) + "Buzz" * (i % 5 == 0) or i)
         
