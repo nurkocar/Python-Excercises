8bfa9de090f6116270c87513f4e0c262244797b6
nurkocar <fatmanurkocar61@gmail.com>
2021-02-21 21:41:19 +0000

    Added repeatedString solution

---
+++ RepeatedString.py
@@ -0,0 +1,13 @@
+def repeatedString(s, n):
+    if s == 'a':
+        return n
+    else:
+        while len(s) <= n:
+            for letter in s:
+                s += letter
+    
+    s = s[:n+1]
+    num_a = s.count('a')
+    return(num_a)
+
+print(repeatedString('aba', 10))
