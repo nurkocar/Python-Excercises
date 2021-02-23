f9d13921df5342828c8b6c90f21cc41672b5574b
nurkocar <fatmanurkocar61@gmail.com>
2021-02-23 00:37:33 +0000

    Added some comments for ListsOperation challenge

--- ListsOperation.py
+++ ListsOperation.py
@@ -17,3 +17,17 @@ for i in range(N):
         lst.pop()
     elif command == 'reverse':
         lst.reverse()
+        
+# 12
+# insert 0 5
+# insert 1 10
+# insert 0 6
+# print
+# remove 6
+# append 9
+# append 1
+# sort
+# print
+# pop
+# reverse
+# print
