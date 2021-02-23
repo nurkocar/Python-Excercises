283f86e2bc57ac6fb0e87ba1d13a52aad11871d4
nurkocar <fatmanurkocar61@gmail.com>
2021-02-23 01:26:10 +0000

    Added FindTheRunnerUpScore and SwapCase challenges

---
+++ FindTheRunnerUpScore.py
@@ -0,0 +1,33 @@
+# n = int(input())
+# arr = map(int, input().split())
+
+# FIRST SOLUTION
+# n = 4
+# arr = [2, 5, 5, 6]
+# arr = set(arr)
+# runner_up = sorted(arr)[-2:-1]
+# print(runner_up)
+# print(runner_up[0])
+
+# instead of number 7 8 and 9 lines number 12 can be used 
+# print(sorted(arr)[-2:-1][0])
+
+# Sample Input 
+
+# 5
+# 2 3 6 6 5
+
+# Sample Output 
+
+# 5
+
+# SECOND SOLUTION
+
+arr = [5, 2, 5, 6, 3]
+
+arr = set(arr)
+print(arr)
+arr = sorted(arr)
+print(arr)
+print(arr[-2])
+
---
+++ sWAP cASE.py
@@ -0,0 +1,4 @@
+def swap_case(s):
+    return s.swapcase()
+
+print(swap_case('HackerRank.com presents "Pythonist 2".'))
