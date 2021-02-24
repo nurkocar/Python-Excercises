5f1aa7922f9ef688c36e906544622653a64defc3
nurkocar <fatmanurkocar61@gmail.com>
2021-02-24 22:48:51 +0000

    Modified NestedList solution

--- Assignment_5.py
+++ Assignment_5.py
@@ -1,5 +1,5 @@
 # 55 e kadar olan Fibonacci listesini verir.
-fibonacci_list = [1,1]
+fibonacci_list = [1, 1]
 toplam = 0
 for i in range(100):
     toplam = fibonacci_list[i] + fibonacci_list[i + 1]
--- NestedList.py
+++ NestedList.py
@@ -1,9 +1,7 @@
 python_students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
 python_students = dict(python_students)
 k = sorted(set((python_students.values())))[1]
-lst = []
-[i for i,j in python_students.items() if j == k]
-
+lst = [i for i,j in python_students.items() if j == k]
 
 for i in sorted(lst):
     print(i)
